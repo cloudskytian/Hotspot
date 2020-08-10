01699a82d504ba52e54eb3c293a00e55e45766b2
cloudskytian <cloudskytian@users.noreply.github.com>
2020-08-11 01:53:50 +0800

    First Upload

---
+++ Hotspot/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Windows.Networking.Connectivity;
+using Windows.Networking.NetworkOperators;
+
+namespace Hotspot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static NotifyIcon notifyIcon;
+        public static ToolStripMenuItem enable = new ToolStripMenuItem("Enable");
+        public static ToolStripMenuItem disable = new ToolStripMenuItem("Disable");
+        public static ToolStripMenuItem exit = new ToolStripMenuItem("Exit");
+        public static EventLogQuery eventLogQuery = new EventLogQuery("Microsoft-Windows-WLAN-AutoConfig/Operational", PathType.LogName, "*[System/EventID=8003]");
+        public static EventLogWatcher eventLogWatcher = new EventLogWatcher(eventLogQuery);
+        public static ConnectionProfile connectionProfile;
+        public static NetworkOperatorTetheringManager networkOperatorTetheringManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Visibility = Visibility.Hidden;
+            enable.Click += new EventHandler(Enable);
+            disable.Click += new EventHandler(Disable);
+            exit.Click += new EventHandler(Exit);
+            notifyIcon = new NotifyIcon
+            {
+                Text = "Hotspot",
+                Visible = true,
+                Icon = Properties.Resources.wlanpref_dll_10408_,
+                ContextMenuStrip = new ContextMenuStrip()
+            };
+            notifyIcon.ContextMenuStrip.Items.Add(enable);
+            notifyIcon.ContextMenuStrip.Items.Add(exit);
+            connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            while (connectionProfile == null)
+            {
+                MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("There is no network connection now, please connect to the network first.\nClick Cancel to exit.\n\n当前暂无网络连接，请先连接一次网络。\n点击取消退出。\n", "No Connection", MessageBoxButton.OKCancel);
+                if (messageBoxResult == MessageBoxResult.Cancel)
+                {
+                    Environment.Exit(0);
+                }
+                connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            }
+            networkOperatorTetheringManager = NetworkOperatorTetheringManager.CreateFromConnectionProfile(connectionProfile);
+            eventLogWatcher.EventRecordWritten += new EventHandler<EventRecordWrittenEventArgs>(HotspotRecover);
+        }
+
+        public static void HotspotRecover(object obj, EventRecordWrittenEventArgs arg)
+        {
+            Thread.Sleep(3000);
+            Task.FromResult(networkOperatorTetheringManager.StartTetheringAsync());
+        }
+
+        public void Enable(object sender, EventArgs e)
+        {
+            eventLogWatcher.Enabled = true;
+            notifyIcon.ContextMenuStrip.Items.Remove(enable);
+            notifyIcon.ContextMenuStrip.Items.Insert(0, disable);
+        }
+
+        public void Disable(object sender, EventArgs e)
+        {
+            eventLogWatcher.Enabled = false;
+            notifyIcon.ContextMenuStrip.Items.Remove(disable);
+            notifyIcon.ContextMenuStrip.Items.Insert(0, enable);
+        }
+
+        public void Exit(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Hotspot
-A tool that automatically re-opens win10 hotspot when the hotspot is closed
+A tool that automatically re-opens win10 hotspot when the hotspot is closed.
+
+A network connection is required when starting the tool, and no network connection is required after the tool is successfully started.
+
+一个在 Win10 热点关闭时自动重新开启热点的小工具。
+
+启动工具时需要有网络连接，成功启动工具后不再需要网络连接。
